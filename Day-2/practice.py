cc791f784481a6934e77e9a0bb74179a8b53aee6
Mehtab19141 <mehtabhoney1@gmail.com>
2025-12-22 00:19:48 -0800

    Implement string operations in practice.py
    Add various string operations and examples in practice.py

---
+++ Day-2/practice.py
@@ -0,0 +1,31 @@
+#print the strring
+print("my name is mehtab")
+print("who is doing the coding")
+
+#Assign string to variable and multiline String
+a = "my name is mehtab"
+print(a)
+a = """my namis is lora
+and i am doing coding"""
+print(a)
+
+#string as array
+a = "mehtab ahmad khan"
+print(a[2])
+
+#looping through string
+for x in "banana":
+  print(x)
+
+#checking of items
+if "khn" in a:
+  print("yes present")
+else:
+  print("not present")
+
+#string lenght
+len(a)
+
+#check present or not present
+print("ahmad" in a)
+print("ahmad" not in a)
